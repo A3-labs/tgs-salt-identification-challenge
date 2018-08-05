79b2b63d697342646fb6f56b4a61e07e9cf46e35
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-08-05 16:14:59 -0400

    Add project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='solution to the TGS Salt Identification Challenge https://www.kaggle.com/c/tgs-salt-identification-ch',
+    author='A3 Labs ',
+    license='MIT',
+)
